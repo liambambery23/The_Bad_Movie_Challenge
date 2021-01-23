858a9a9e8cb0027f4a934049f7afb7567048ffa2
Spencer Rosio <spencerrosio3d@gmail.com>
2021-01-23 13:24:24 -0600

    added delete routes to challenge and watched controllers

--- controllers/watchedMovies.js
+++ controllers/watchedMovies.js
@@ -9,11 +9,25 @@ module.exports = function (app) {
     console.log(res);
     db.Watched.create({
       userId: req.user.id,
-      movieId: req.body.movieId    
+      movieId: req.body.movieId
     }).then((res) => {
       console.log(res);
-    }); 
+    });
   });
+
+  app.delete("/api/watched/:id", (req) => {
+    const movieId = req.params.id;
+    const userId = req.user.id;
+
+    db.Watched.destroy({
+      where: {
+        userId: userId,
+        movieId: movieId
+      }
+    }).then((res) => {
+      console.log(res);
+    });
+  });
+
 };
 
- 
