a29d96fb3835dea7ccaa0f006aceadd44e9baa35
Liam B <71141670+liambambery23@users.noreply.github.com>
2021-01-23 13:50:50 -0500

    created post for challenge table

--- controllers/challenge.js
+++ controllers/challenge.js
@@ -2,9 +2,13 @@ const db = require("../models/");
 
 module.exports = function (app) {
   app.post("/api/challenge/", (req, res) => {
+      console.log(res);
     db.Challenge.create({
-        
-    })
+        userId: req.user.id,
+        movieId: req.body.movieId    
+    }).then((res) => {
+        console.log(res);
+    }) 
   });
 };
 
